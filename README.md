c2b00312ac5ed535e8fe1be41faa903c589f9e12
Dante <dante_padley@csu.fullerton.edu>
2022-04-08 22:47:11 -0700

    final final

--- README.md
+++ README.md
@@ -1,38 +1,46 @@
 # wordle-project
-Nathan Eduvala, Marcellus Jones, Hoang Long Nguyen, Dante Padley
+## Nathan Eduvala, Marcellus Jones, Hoang Long Nguyen, Dante Padley
 
 
 1. Initialization
 To get the list of wordle answers, run the following:
 
-curl --silent https://www.nytimes.com/games/wordle/main.bfba912f.js |
-sed -e 's/^.*var Ma=//' -e 's/,Oa=.*$//' -e 1q > ./fastapi/answer-checking/answers.json
+`curl --silent https://www.nytimes.com/games/wordle/main.bfba912f.js |
+sed -e 's/^.*var Ma=//' -e 's/,Oa=.*$//' -e 1q > ./fastapi/answer-checking/answers.json`
 
 To initialize the answers db, navigate to the /fastapi/answer-checking and run the following:
-mkdir db
+`mkdir db
 cd db
 sqlite3 answers.db
 CREATE TABLE answers(id INTEGER PRIMARY KEY, word TEXT)
-.quit
+.quit`
 
 To initialize the answers db, navigate to the /fastapi/word-validation and run the following:
-mkdir db
+
+`mkdir db
 cd db
 sqlite3 words.db
 CREATE TABLE words(word TEXT)
 .quit
-
+`
 
 To populate the answers db, run the /fastapi/answer-checking/
 populatedb.py script:
-python3 ./fastapi/answer-checking/populatedb.py
+
+`python3 ./fastapi/answer-checking/populatedb.py`
 
 To get the dictionary from linux and trim it down to only valid words for wordle, run the command:
-source ./fastapi/word-validation/script.sh
+source 
 
-To populate the words db, run the /fastapi/word-validation/populatewords.py script:
-python3 ./fastapi/word-validation/populatewords.py
+`./fastapi/word-validation/script.sh`
 
+To populate the words db, run the 
+
+`/fastapi/word-validation/populatewords.py`
+
+script:
+
+`python3 ./fastapi/word-validation/populatewords.py`
 
 
 
--- fastapi/word-validation/api.py
+++ fastapi/word-validation/api.py
@@ -17,14 +17,8 @@ def validateWord(word):
     pattern = re.compile("^[\x61-\x7A]{5}$")
     if (pattern.match(word)):
         db = sqlite3.Connection('./word-validation/db/words.db')
-        #db.execute("CREATE TABLE IF NOT EXISTS words (id INT PRIMARY KEY, word TEXT NOT NULL)")
         cur = db.cursor()
         cur.execute("SELECT * FROM words WHERE word = ?", [word])
-        # f = open("./word-validation/dictionary.txt", "r")
-        # y = f.read().split("\n")
-        # f.close()
-        # for x in y:
-        #     db.execute('INSERT INTO words(word) VALUES(?)', [x])
         
         print(cur.arraysize)
         dbWords = cur.fetchall()
@@ -34,21 +28,12 @@ def validateWord(word):
         
         return{"word": word, "valid": "true"}
 
-    #     f = open("./word-validation/dictionary.txt", "r")
-    #     words = []
-    #     words = f.read().split("\n")
-    #     if word in words:
-    #     # at this point, we know the word contains 5 lowercase letters
-    #         return {"word": word, "valid": "true"}
-    # return {"word": word, "valid": "false"}
-
 @app.post("/word/add/")
 def addWord(word: str):
 
     pattern = re.compile("^[\x61-\x7A]{5}$")
     if (pattern.match(word)):
         db = sqlite3.Connection('./word-validation/db/words.db')
-        #db.execute("CREATE TABLE IF NOT EXISTS words (id INT PRIMARY KEY, word TEXT NOT NULL)")
         cur = db.cursor()
         cur.execute("INSERT INTO words(word) VALUES(?)", [word])
         db.commit()
@@ -59,22 +44,7 @@ def deleteWord(word:str):
     pattern = re.compile("^[\x61-\x7A]{5}$")
     if (pattern.match(word)):
         db = sqlite3.Connection('./word-validation/db/words.db')
-        #db.execute("CREATE TABLE IF NOT EXISTS words (id INT PRIMARY KEY, word TEXT NOT NULL)")
         cur = db.cursor()
         cur.execute("DELETE FROM words WHERE word = ?", [word])
         db.commit()
         return 
-
-# import os
-# @app.get("/word2/isvalid/{word}")
-# def validateWord2(word):
-    
-#     pattern = re.compile("^[\x61-\x7A]{5}$")
-#     if (pattern.match(word)):
-#         db = sqlite3.Connection("./word-validation/db/words.db")
-#         db.execute("CREATE TABLE IF NOT EXISTS words2 (id INT PRIMARY KEY, word TEXT NOT NULL)")
-#         stream = os.popen("cat /usr/share/dict/words | grep -P  '^[\x61-\x7A]{5}$'")
-#         print("king king")
-#         print(stream)
-        
-#     return True
