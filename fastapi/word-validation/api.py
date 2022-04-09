cf57a1e6225e5b0e0525597dda5931dae0227e20
Dante <dante_padley@csu.fullerton.edu>
2022-04-08 17:04:14 -0700

    words db populated

--- fastapi/word-validation/api.py
+++ fastapi/word-validation/api.py
@@ -1,10 +1,19 @@
+import re
+import sqlite3
+
 from fastapi import FastAPI
+from string import ascii_lowercase, punctuation
+
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-@app.post("/word/isvalid/{word}")
+@app.get("/word/isvalid/{word}")
 def validateWord(word):
-    return {"word": word}
+    pattern = re.compile("^[\x61-\x7A]{5}$")
+    if (pattern.match(word)):
+        # at this point, we know the word contains 5 lowercase letters
+        return{"word": word, "valid": "true"}
+    return {"word": word, "valid": "false"}
