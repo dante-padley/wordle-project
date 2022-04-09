7cd90d655b38ebe0ea11024c53364919f89a76cf
Hoang Long <hlong24399@gmail.com>
2022-04-08 17:26:02 -0700

    add fileinput to  answer-checking module

--- fastapi/answer-checking/api.py
+++ fastapi/answer-checking/api.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
 app = FastAPI()
 
+import fileinput
+word_dictionary = []
+for line in fileinput.input():
+    word_dictionary.append(line)
+print(word_dictionary)
+
 @app.get("/")
 async def root():
     return {"message": "Goodbye World"}
